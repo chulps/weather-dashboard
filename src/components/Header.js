3a659f9171b1d800de4910fb9d3b1f8715aef404
Chuck Howard <chuckoward@gmail.com>
2024-05-17 11:39:19 +0900

    new icons for the weather and also added light/dark theme toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../images/logo.gif";
 import RotatingText from "./RotatingText";
 import "../css/header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSun,
+  faMoon
+} from "@fortawesome/free-solid-svg-icons";
+
 
 const Header = () => {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
+
   return (
     <header>
       <a href="https://chulps.github.io/react-gh-pages/" className="logo-container">
-          <RotatingText />
-          <img className="logo" src={logo} alt="Chuck Howard" />
+        <RotatingText />
+        <img className="logo" src={logo} alt="Chuck Howard" />
       </a>
+      <div className="header-right">
+        <button onClick={toggleTheme} className="theme-toggle">
+          {theme === "dark" ? <FontAwesomeIcon icon={faSun} /> : <FontAwesomeIcon icon={faMoon} />}
+        </button>
+      </div>
     </header>
   );
 };
